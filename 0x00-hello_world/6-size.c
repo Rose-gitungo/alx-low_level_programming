6b33f0919fe1b476cd0e9961af09b591e206f515
rosegitungo <gitungoros3gmail.com>
2022-10-14 15:04:21 -0700

    5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+*main- ENtry point
+*
+*return: Always 0 (Success)
+*/
+int main(void)
+{
+    int a;
+    long int b;
+    long long int c;
+    char d;
+    float f;
+
+    printf("Sie of a char : %lu byte(s)\n", sizeof(d));
+    printf('size of int: %lu byte\n', sizeof(a));
+    printf("size of long long int :%lu byte\n", sizeof(c));
+    printf("size of float: %lu bytes\n", sizeof(f));
+    printf("size of long int:%lu byte\n", sizeof(b));
+    return (0);
+ }
