b24562ff005f194c1c8cd2e0a7f1deee027aa13b
rosegitungo <gitungoros3gmail.com>
2022-10-14 15:20:07 -0700

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-    char str1[]=' "Programming is like building a multilingual puzzle"\n'
-    puts(str1);
-    return 0;
-  }
+  puts(' "Programming is like building a multilingual puzzle\n" ');
+  return(0);
+}
