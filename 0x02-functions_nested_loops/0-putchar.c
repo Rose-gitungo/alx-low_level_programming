dc2f46bf02449c53aaae3f111c8ee90ed9a3ac20
rosegitungo <gitungoros3gmail.com>
2022-10-18 15:47:47 -0700

    9

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main -Entry point
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = '_';
+char a = 'p';
+char a = 'u';
+char a = 't';
+char a = 'c';
+char a = 'h';
+char a = 'a';
+char a = 'r';
+char a = '\n';
+for (a = '_'; a <= '\n'; a++)
+putchar(a);
+printf("_putchar\n");
+return (0);
+}
