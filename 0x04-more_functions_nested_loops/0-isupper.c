8550601fedcb5b420701f328298bc8faff8abcd1
rosegitungo <gitungoros3gmail.com>
2022-10-24 16:03:02 -0700

    2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 char c;
-c='A';
-printf("%c: %d\n", c, _isupper(c));
-c='a';
-printf("%c; %d\n", c, _isupper(c));
+c = 'A';
+printf("%c: %d\n", c, isupper(c));
+c = 'a';
+printf("%c; %d\n", c, isupper(c));
 return (0);
-} 
+}
